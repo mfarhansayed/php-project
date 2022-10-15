e82ebaa5fbe84b9c7a7da8b691f36d7d7d410f35
Mohammed Farhan Sayed <84790596+mfarhansayed@users.noreply.github.com>
2022-10-16 02:27:33 +0530

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
 <?php
-echo 'DevOps!';
+$uid = $_POST['userid'];
+$pw = $_POST['password'];
+ 
+if($uid == 'ben' and $pw == 'ben23')
+{    
+    session_start();
+    $_SESSION['sid']=session_id();
+    echo "Logged in successfully";
+}
 ?>
+ 
+</body>
+</html>
